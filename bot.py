89f122e34c3ab84e974fd7d75b07f08c4c498283
hedred <128807947+hedred-z@users.noreply.github.com>
2024-11-07 09:15:20 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,32 +1,31 @@
 import logging
 from datetime import datetime, timedelta, time
-from telegram import Update, ReplyKeyboardMarkup
+from telegram import Update, ReplyKeyboardMarkup, KeyboardButton
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, CallbackContext
 
 from config import API_TOKEN
 
-# Идентификатор администратора
 ADMIN_ID = 954053674
-
-# Хранилище для ссылок (можно заменить на базу данных)
 video_links_by_day = {}
-
-# Состояние пользователя: последний пройденный день и время доступа к следующему дню
 user_progress = {}
-
-# Состояние запроса ссылок
 video_link_input = {}
 
-# Логирование
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-# Функция для отображения клавиатуры с учетом роли пользователя
 def get_keyboard(user_id):
     if user_id == ADMIN_ID:
-        # Клавиатура с кнопкой "Админ-панель" только для администратора
-        return ReplyKeyboardMarkup([["Админ-панель"]], resize_keyboard=True)
-    return ReplyKeyboardMarkup([["День 1"]], resize_keyboard=True)
+        days_keyboard = []
+        for day in range(1, 46):
+            button_text = f"День {day}"
+            if day in video_links_by_day:
+                button_text += " ✅"
+            days_keyboard.append([KeyboardButton(button_text)])
+        reply_markup = ReplyKeyboardMarkup(days_keyboard, resize_keyboard=True)
+    else:
+        day_buttons = [[f"День {i}"] for i in range(1, len(user_progress) + 2)]
+        reply_markup = ReplyKeyboardMarkup(day_buttons, resize_keyboard=True)
+    return reply_markup
 
 async def start(update: Update, context: CallbackContext) -> None:
     user_id = update.message.from_user.id
@@ -37,7 +36,6 @@ async def start(update: Update, context: CallbackContext) -> None:
 
 async def admin_panel(update: Update, context: CallbackContext) -> None:
     if update.message.from_user.id == ADMIN_ID:
-        # Панель администратора с выбором дня
         days_keyboard = [[str(day)] for day in range(1, 46)]
         reply_markup = ReplyKeyboardMarkup(days_keyboard, one_time_keyboard=True, resize_keyboard=True)
         await update.message.reply_text("Выберите день для добавления видео:", reply_markup=reply_markup)
@@ -45,38 +43,32 @@ async def admin_panel(update: Update, context: CallbackContext) -> None:
         await update.message.reply_text("У вас нет доступа к этой функции.")
 
 async def ask_video_count(update: Update, context: CallbackContext) -> None:
-    # Сохраняем выбранный день в контексте
-    day = int(update.message.text)
+    day = int(update.message.text.split()[1])
     context.user_data['day'] = day
-    video_link_input[day] = 0  # Сбросим счетчик видео
+    video_link_input[day] = 0
     await update.message.reply_text(f"Сколько видео вы хотите добавить для дня {day}?")
 
 async def save_video_link(update: Update, context: CallbackContext) -> None:
     user_message = update.message.text
     user_id = update.message.from_user.id
 
-    # Если это ссылка на видео
     if user_message.startswith('http'):
         day = context.user_data.get('day')
         if day is not None:
-            # Проверяем, сколько видео нужно для дня
             video_count = context.user_data.get('video_count', 0)
             if day not in video_links_by_day:
                 video_links_by_day[day] = []
             
-            # Добавляем ссылку
             video_links_by_day[day].append(user_message)
             video_link_input[day] += 1
 
-            # Проверяем, сколько еще ссылок нужно
             if video_link_input[day] < video_count:
                 await update.message.reply_text(f"Добавьте ссылку на {video_link_input[day] + 1} видео.")
             else:
                 await update.message.reply_text(f"Все {video_count} видео добавлены для дня {day}.")
-                # Сбросим текущие данные
                 context.user_data['day'] = None
                 video_link_input[day] = 0
-                context.user_data['video_count'] = None  # Сбросим количество видео
+                context.user_data['video_count'] = None
         else:
             await update.message.reply_text("Ошибка: день не выбран.")
     else:
@@ -86,23 +78,23 @@ async def start_day(update: Update, context: CallbackContext) -> None:
     user_id = update.message.from_user.id
     current_time = datetime.now()
     
-    # Проверка прогресса пользователя и времени доступа
     if user_id in user_progress:
         last_day, next_available_time = user_progress[user_id]
         if current_time < next_available_time:
             remaining_time = next_available_time - current_time
             hours, remainder = divmod(remaining_time.seconds, 3600)
             minutes = remainder // 60
-            await update.message.reply_text(f"Следующий день будет доступен через {hours} часов и {minutes} минут.")
+            time_string = f"{hours} часов" if hours > 0 else ""
+            time_string += f" {minutes} минут" if minutes > 0 else ""
+            await update.message.reply_text(f"Следующий день будет доступен через {time_string}.")
             return
 
-    # Проверка, что день доступен не раньше 7 утра
     if current_time.time() < time(7, 0):
         time_until_access = datetime.combine(current_time.date(), time(7, 0)) - current_time
-        await update.message.reply_text(f"День будет доступен через {time_until_access.seconds // 3600} часов и {time_until_access.seconds % 3600 // 60} минут.")
+        time_string = f"{time_until_access.seconds // 3600} часов {time_until_access.seconds % 3600 // 60} минут"
+        await update.message.reply_text(f"День будет доступен через {time_string}.")
         return
     
-    # Отправляем видео для текущего дня
     day = user_progress.get(user_id, (0, None))[0] + 1
     if day in video_links_by_day:
         await update.message.reply_text(f"День {day} - Начинаем просмотр видео!")
@@ -113,7 +105,6 @@ async def start_day(update: Update, context: CallbackContext) -> None:
     else:
         await update.message.reply_text("Видео для этого дня пока не добавлено.")
     
-    # Обновляем прогресс пользователя
     next_available_time = current_time + timedelta(days=1)
     user_progress[user_id] = (day, next_available_time)
 
@@ -126,17 +117,14 @@ async def watched(update: Update, context: CallbackContext) -> None:
 def main() -> None:
     application = Application.builder().token(API_TOKEN).build()
 
-    # Обработчики команд
     application.add_handler(CommandHandler("start", start))
     application.add_handler(MessageHandler(filters.Regex("Админ-панель"), admin_panel))
-    application.add_handler(MessageHandler(filters.Regex(r"^\d+$"), ask_video_count))  # Выбор дня
-    application.add_handler(MessageHandler(filters.Regex(r'http'), save_video_link))  # Проверка на ссылку
+    application.add_handler(MessageHandler(filters.Regex(r"День \d+"), ask_video_count))
+    application.add_handler(MessageHandler(filters.Regex(r'http'), save_video_link))
     application.add_handler(MessageHandler(filters.Regex("День 1"), start_day))
     application.add_handler(MessageHandler(filters.Regex("✅ Я посмотрел"), watched))
     
-    # Запуск бота
     application.run_polling()
 
 if __name__ == "__main__":
     main()
-    
